28c781da080ec7261a4972d7cb09f79446ccc40e
nicoliprogramer <imotivacionesf@gmail.com>
2023-04-30 20:03:39 -0300

    modified-delete

--- models/chatModel.js
+++ models/chatModel.js
@@ -13,17 +13,3 @@ const schemaChat = new mongoose.Schema({
 const modelChat = mongoose.model(collectionChat, schemaChat);
 
 export default modelChat;
-
-// const schemaChat = new mongoose.Schema({
-//   author: {
-//     email: String,
-//     nombre: String,
-//     apellido: String,
-//     edad: Number,
-//     alias: String,
-//     avatar: String,
-//   },
-//   text: String,
-//   fyh: String,
-//   id: Number,
-// });
